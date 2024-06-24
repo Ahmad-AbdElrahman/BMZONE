fa25ecb6b3a9821ba2dfcbf47ffd82efb88d6cd9
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-06-24 21:55:47 +0300

    adding the rest of html pages to templates and fixing static images

--- core/views.py
+++ core/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def sproduct(request):
+    return render(request, 'core/sproduct.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def cart(request):
+    return render(request, 'core/cart.html')
